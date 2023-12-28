92f9847caa17f32f3f963dc8c981a1cb3970f132
monahvi <nahvi.moghaddam@gmail.com>
2023-12-28 12:03:46 +0000

    next step

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -6,7 +6,26 @@ order by 3,4
 --From PortfolioProject..CovidVaccinations
 --order by 3,4
 
+--Select Data that we are going be using
+
 select location, date, total_cases, new_cases,total_deaths,population
 From PortfolioProject..CovidDeaths
 order by 1,2
 
+
+-- Looking at Total Cases vs Total Deaths
+-- Shows likelihood of dying if you contract covid in your country
+
+Select location, date, total_cases, total_deaths,(total_deaths/total_cases) *100 As DeathPersentage
+From PortfolioProject..CovidDeaths
+Where location like '%United kingdom%'
+order by 1,2
+
+
+-- Looking at Total Cases vs Population
+-- Shows what percentage of population got Covid!
+
+Select location, date, population, total_cases, (total_cases/population) *100 As DeathPersentage
+From PortfolioProject..CovidDeaths
+Where location like '%United kingdom%'
+order by 1,2
